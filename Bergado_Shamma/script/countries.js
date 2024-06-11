477a1cdc7d8b84febc21ff5e5ee16297e19b1ea0
bergado-shamma <shabergado.22@gmail.com>
2024-06-11 08:20:38 +0800

    add changes

--- Bergado_Shamma/script/countries.js
+++ Bergado_Shamma/script/countries.js
@@ -1,5 +1,5 @@
 function searchCountry() {
-  let countryName = document.getElementById("country_input").value.trim();
+  const countryName = document.getElementById("country_input").value.trim();
   if (!countryName) {
     document.getElementById("country_details").innerHTML =
       "<p>Please enter a country name.</p>";
@@ -16,49 +16,68 @@ function searchCountry() {
       return response.json();
     })
     .then(function (countryData) {
-      let country = countryData[0];
-      let details = `
+      const country = countryData[0];
+      const details = `
               <h2>Country Details - ${country.name.common}</h2>
               <img src="${country.flags.svg}" alt="Flag of ${
         country.name.common
       }
                        " width="100">
-              <p><strong>Area:</strong> ${
-                country.area
-                  ? country.area.toLocaleString() + " square kilometers"
-                  : "N/A"
-              }</p>
-              <p><strong>Population:</strong> ${
-                country.population ? country.population.toLocaleString() : "N/A"
-              }</p>
-              <p><strong>Region:</strong> ${
-                country.region ? country.region : "N/A"
-              }</p>
-              <p><strong>Capital:</strong> ${
-                country.capital ? country.capital[0] : "N/A"
-              }</p>
-              <p><strong>Currency:</strong> ${
-                country.currencies
-                  ? Object.values(country.currencies).join(", ")
-                  : "N/A"
-              }</p>
-              <p><strong>Calling Code:</strong> ${
-                country.callingCodes ? country.callingCodes[0] : "N/A"
-              }</p>
-              <p><strong>Timezones:</strong> ${
-                country.timezones ? country.timezones.join(", ") : "N/A"
-              }</p>
-              <p><strong>Native Name:</strong> ${
-                country.name.nativeName
-                  ? Object.values(country.name.nativeName).join(", ")
-                  : "N/A"
-              }</p>
-              <p><strong>Official Languages:</strong> ${
-                country.languages
-                  ? Object.values(country.languages).join(", ")
-                  : "N/A"
-              }</p>
-            `;
+              <p>
+                <strong>Area:</strong> ${
+                  country.area
+                    ? country.area.toLocaleString() + " square kilometers"
+                    : "N/A"
+                }
+              </p>
+              <p>
+                <strong>Population:</strong> ${
+                  country.population
+                    ? country.population.toLocaleString()
+                    : "N/A"
+                }
+              </p>
+              <p>
+                <strong>Region:</strong> ${
+                  country.region ? country.region : "N/A"
+                }
+              </p>
+              <p>
+                <strong>Capital:</strong> ${
+                  country.capital ? country.capital[0] : "N/A"
+                }
+              </p>
+              <p>
+                <strong>Currency:</strong> ${
+                  country.currencies
+                    ? Object.values(country.currencies).join(", ")
+                    : "N/A"
+                }
+              </p>
+              <p>
+                <strong>Calling Code:</strong> ${
+                  country.callingCodes ? country.callingCodes[0] : "N/A"
+                }
+              </p>
+              <p>
+                <strong>Timezones:</strong> ${
+                  country.timezones ? country.timezones.join(", ") : "N/A"
+                }
+              </p>
+              <p>
+                <strong>Native Name:</strong> ${
+                  country.name.nativeName
+                    ? Object.values(country.name.nativeName).join(", ")
+                    : "N/A"
+                }
+              </p>
+              <p>
+                <strong>Official Languages:</strong> ${
+                  country.languages
+                    ? Object.values(country.languages).join(", ")
+                    : "N/A"
+                }
+              </p>`;
       document.getElementById("countryDetails").innerHTML = details;
 
       return fetch("https://restcountries.com/v3.1/region/" + country.region);
@@ -70,22 +89,23 @@ function searchCountry() {
       return response.json();
     })
     .then(function (regionData) {
-      let region = regionData[0].region;
-      let sameRegionCountriesList = regionData
+      const region = regionData[0].region;
+      const sameRegionCountriesList = regionData
         .map(function (c) {
           return `
-                <div class="country-card">
-                  <img src="${c.flags.svg}" alt="Flag of ${c.name.common}"
-                   width="50">
-                  <p>${c.name.common}</p>
-                </div>
-              `;
+                  <div class="country-card">
+                    <img 
+                      src="${c.flags.svg}" 
+                      alt="Flag of ${c.name.common}"
+                      width="50">
+                    <p>${c.name.common}</p>
+                  </div>
+                  `;
         })
         .join("");
       document.getElementById("same_region_countries").innerHTML = `
               <h2>Countries in the Same Region (${region})</h2>
-              <div class="country-list">${sameRegionCountriesList}</div>
-            `;
+              <div class="country-list">${sameRegionCountriesList}</div>`;
     })
     .catch(function (error) {
       console.error("Error fetching data:", error);
